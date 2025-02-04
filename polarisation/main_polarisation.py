72b8b6af6b09d169c72b9bf2cd31bee8218f9344
Mathieu Bouchard <mabo1165@ulaval.ca>
2025-02-03 22:24:14 -0500

    Modification figure

--- polarisation/main_polarisation.py
+++ polarisation/main_polarisation.py
@@ -23,6 +23,21 @@ intensite_TM = np.array([19.5, 21.4, 22.9, 24.4, 29.7,
 
 ax1.plot(angle_incident, intensite_TE,
         angle_incident, intensite_TM, '--')
+ax1.axhline(0, color='gray', linestyle=':', linewidth=1)
+ax1.axvline(0, color='gray', linestyle=':', linewidth=1)
+#ax1.set_title("Intensité réfléchie en fonction de l'angle d'incidence du laser")
+ax1.set_ylabel(r"Intensité [$\mu$A]")
+ax1.set_xlabel("Angle d'incidence")
+I_max_ax1 = 250  
+#ax1_right = ax1.twinx()
+#ax1_right.set_ylabel('Transmission [%]')
+#ax1_right.set_ylim(0, (ax1.get_ylim()[1] / I_max_ax1) * 100)
+#ax1_right.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.0f}%"))
+
+ax1.xaxis.set_major_locator(tck.MultipleLocator(10))
+ax1.yaxis.set_major_locator(tck.MultipleLocator(50))  
+ax1.grid(True, which='major', linestyle=':', linewidth=0.5)
+
 
 '''
 Graphiquement, on voit que l'angle de Brewster du plexiglas est 55°.
@@ -36,6 +51,13 @@ fig2 = plt.figure()
 ax2 = fig2.add_subplot(1, 1, 1)
 ax2.xaxis.set_minor_locator(tck.AutoMinorLocator())
 ax2.yaxis.set_minor_locator(tck.AutoMinorLocator())
+ax2.axhline(0, color='gray', linestyle=':', linewidth=1)
+ax2.axvline(0, color='gray', linestyle=':', linewidth=1)
+#ax2.set_title("Intensité en fonction de l'angle du prisme de Glan-Thompson")
+ax2.xaxis.set_major_locator(tck.MultipleLocator(30))
+ax2.yaxis.set_major_locator(tck.MultipleLocator(25))  
+ax2.grid(True, which='major', linestyle=':', linewidth=0.5)
+
 
 angle_prisme = np.arange(0, 190, 10)
 
@@ -48,12 +70,29 @@ intensite_transmise = np.array([153, 142.5, 128.5, 106.7, 81.8, 53.9,
 ##                     0.3, 0.3, 0.5, 0.8, 0.5, 0.8, 0.8])
 
 ax2.plot(angle_prisme, intensite_transmise)
+I_max_ax2 = 315  
+ax2_right = ax2.twinx()
+ax2_right.set_ylabel('Transmission [%]')
+ax2_right.set_ylim(0, (ax2.get_ylim()[1] / I_max_ax2) * 100)
+ax2_right.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.0f}%"))
+ax2.set_xlabel(r'Angle du polariseur [$\degree$]')
+ax2.set_ylabel(r'Amplitude [$\mu$A]')
 
 # Caratérisation des lames à retard
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(1, 1, 1)
 ax3.xaxis.set_minor_locator(tck.AutoMinorLocator())
 ax3.yaxis.set_minor_locator(tck.AutoMinorLocator())
+ax3.axhline(0, color='gray', linestyle=':', linewidth=1)
+ax3.axvline(0, color='gray', linestyle=':', linewidth=1)
+#ax3.set_title("Intensité du laser transmise à travers un polariseur (Glan-Thompson ou dichroïque) en fonction de son angle pour différentes orientations d’une lame à retard (22,5° et 45°).", wrap=True)
+ax3.set_xlabel(r'Angle du polariseur [$\degree$]')
+ax3.set_ylabel(r'Amplitude [$\mu$A]')
+ax3.xaxis.set_major_locator(tck.MultipleLocator(30))
+ax3.yaxis.set_major_locator(tck.MultipleLocator(25))  
+ax3.grid(True, which='major', linestyle=':', linewidth=0.5)
+
+
 
 angle_polariseur = np.arange(0, 190, 20)
 
@@ -69,11 +108,17 @@ intensite_quart_22_5 = np.array([38.5, 74.8, 114.4, 138.3, 143.7,
 intensite_quart_45 = np.array([90.3, 90.8, 93.7, 92.4, 95.6,
                                83.0, 80.3, 76.3, 84.4, 86.10])
 
-ax3.plot(angle_polariseur, intensite_demi_22_5,
-         angle_polariseur, intensite_demi_45, '--',
-         angle_polariseur, intensite_quart_22_5, '-.',
-         angle_polariseur, intensite_quart_45, ':')
+ax3.plot(angle_polariseur, intensite_demi_22_5, label="Lame demi onde 22,5°")
+ax3.plot(angle_polariseur, intensite_demi_45, '--', label="Lame à demi onde 45°")
+ax3.plot(angle_polariseur, intensite_quart_22_5, '-.', label="Lame quart d'onde 22,5°")
+ax3.plot(angle_polariseur, intensite_quart_45, ':', label="Lame quart d'onde 45°")
+ax3.legend(loc="best")
 
+I_max_ax3 = 315  
+ax3_right = ax3.twinx()
+ax3_right.set_ylabel('Transmission [%]')
+ax3_right.set_ylim(0, (ax3.get_ylim()[1] / I_max_ax3) * 100)
+ax3_right.yaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{x:.0f}%"))
 
 ##angle = np.arange(0, 200, 20)
 ##I_demi_22_5 = np.array([55.9, 123.3, 172.1, 199.6,
