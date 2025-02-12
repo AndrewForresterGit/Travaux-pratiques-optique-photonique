46c8155fa610ee57959266d402a38d1aced8bd4c
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2025-02-11 23:07:03 -0500

    encore formatage graphs fibre

--- Fibre/main.fibre.py
+++ Fibre/main.fibre.py
@@ -5,14 +5,15 @@ from getcsvData import getcsvData_dict as getcsv
 
 
 class fibre:
-    def __init__(self, type_mode, path):
+    def __init__(self, type_mode, path, a):
         self.path = path
         self.type_mode = type_mode
         data = getcsv(path)
         keys = list(data.keys())
         self.position_raw = np.array(data[keys[0]])
-        self.position_norm = np.linspace(-62.5, 62.5, len(self.position_raw))
+        self.position_norm = np.linspace(-a, a, len(self.position_raw))
         self.intensité = np.array(data[keys[1]])
+        self.a = a
 
         # initialisaion de la figure
         self.fig = None
@@ -24,7 +25,7 @@ class fibre:
         
         self.ax.xaxis.set_minor_locator(tck.AutoMinorLocator())
         self.ax.yaxis.set_minor_locator(tck.AutoMinorLocator())
-        self.ax.xaxis.set_major_locator(tck.MultipleLocator(20))
+        self.ax.xaxis.set_major_locator(tck.MultipleLocator(int(self.a/3)))
         self.ax.yaxis.set_major_locator(tck.MultipleLocator(.1))
         self.ax.set_xlabel(xlabel)
         self.ax.set_ylabel(ylabel)
@@ -32,23 +33,23 @@ class fibre:
 
 
 # Les monomodes
-mono_s = fibre("mono", "Data/Mono_square_1.csv")
-mono_x = fibre("mono", "Data/Mono_x_1.csv")
-mono_y = fibre("mono", "Data/Mono_y_1.csv")
+mono_s = fibre("mono", "Data/Mono_square_1.csv", a=3.6)
+mono_x = fibre("mono", "Data/Mono_x_1.csv", a=3.6)
+mono_y = fibre("mono", "Data/Mono_y_1.csv", a=3.6)
 
 # Moyennement multimodes
-moyen_s = fibre("moyen", "Data/Moyen_square_1.csv")
-moyen_x = fibre("moyen", "Data/Moyen_x_1.csv")
-moyen_y = fibre("moyen","Data/Moyen_y_1.csv")
+moyen_s = fibre("moyen", "Data/Moyen_square_1.csv", a=25)
+moyen_x = fibre("moyen", "Data/Moyen_x_1.csv", a=25)
+moyen_y = fibre("moyen","Data/Moyen_y_1.csv", a=25)
 
 # Hautement multimodes
-haut_s = fibre("haut","Data/Multi_square_1.csv")
-haut_x = fibre("haut","Data/Multi_x_1.csv")
-haut_y = fibre("haut","Data/Multi_y_1.csv")
+haut_s = fibre("haut","Data/Multi_square_1.csv", a=62.5)
+haut_x = fibre("haut","Data/Multi_x_1.csv", a=62.5)
+haut_y = fibre("haut","Data/Multi_y_1.csv", a=62.5)
 
-mono_x.plot()
-moyen_x.plot()
-haut_x.plot(r"Position radiale [$\mu$m]", r"Intensité [%]", )
+mono_x.plot(r"Position radiale [$\mu$m]", r"Intensité [%]")
+moyen_x.plot(r"Position radiale [$\mu$m]", r"Intensité [%]")
+haut_x.plot(r"Position radiale [$\mu$m]", r"Intensité [%]")
 
 ##plt.legend()
 plt.show()
