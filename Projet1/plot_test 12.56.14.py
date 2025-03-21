a5e1600977b840846537dfd4680e6d0d551c720d
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2025-03-21 12:48:35 -0400

    baudrate test

--- Projet1/plot_test 12.56.14.py
+++ Projet1/plot_test 12.56.14.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    temps = np.load('temps_image.npy')
+    temps = np.load('temps_baud_test_300.npy')
 ##    position_x = np.load('position_x.npy')
-    tension = np.load('tension_image.npy')
+    tension = np.load('tension_baud_test_300.npy')
     plt.plot(temps, tension)
 
     
