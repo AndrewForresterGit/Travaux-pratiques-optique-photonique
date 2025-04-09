39d6167c73f66888685ce3a271a16d83b3b05109
Mathieu Bouchard <mabo1165@ulaval.ca>
2025-04-09 09:55:30 -0400

    kjbkjb

--- projet2/code_filter.m
+++ projet2/code_filter.m
@@ -4,8 +4,8 @@ v = moku.data(:, 2);
 
 %% Paramètres d'échantillonnage
 % Calcul de la fréquence d'échantillonnage
-fs = 1 / (t(2) - t(1)); 
-fprintf('Fréquence d''échantillonnage: %.2f Hz\n', fs);
+f = 1 / (t(2) - t(1)); 
+fprintf('Fréquence d''échantillonnage: %.2f Hz\n', f);
 
 %% Conception d'un filtre passe-bande Butterworth
 low_cutoff  = 100;    % par exemple, 20 Hz
@@ -15,7 +15,7 @@ high_cutoff = 3000; % par exemple, 20 kHz (vérifiez que cela correspond à votr
 order = 4; 
 
 % Conception du filtre
-[b, a] = butter(order, [low_cutoff, high_cutoff] / (fs/2), 'bandpass');
+[b, a] = butter(order, [low_cutoff, high_cutoff] / (f/2), 'bandpass');
 
 %% Application du filtre
 
@@ -42,18 +42,18 @@ xlabel('Temps (s)');
 ylabel('Tension (V)');
 grid on;
 
-subplot(4,1,2)
+subplot(4,1,3)
 plot(f, abs(FFT_signal));
 title('Spectre du signal mesuré');
 xlabel('Fréquence (Hz)');
 ylabel('Amplitude');
 grid on;
 
-subplot(3,1,2)
-plot(f, abs(FFT_dark));
-title('Spectre du signal dark');
+subplot(4,1,4)
+plot(f, abs(FFT_signal_filtered));
+title('Spectre du signal filtré');
 xlabel('Fréquence (Hz)');
 ylabel('Amplitude');
 grid on;
 
-audiowrite("test_440.wav", v, round(fs));
+audiowrite("test_440.wav", v, round(f));
