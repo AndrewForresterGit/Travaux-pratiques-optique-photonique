632c36b4fdda39abbeaf0c4640f90b8c43cbf668
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2025-04-18 15:18:46 -0400

    debut analyse projet2

---
+++ Projet2/python_scripts/analyse.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+"""
+Author: Andrew Forrester
+Created: 2025-04-18
+Description: ...
+"""
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
+
