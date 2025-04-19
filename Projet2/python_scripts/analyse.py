3fa051ebef0599d99891fcede375ed3bc1771ecd
Andrew Forrester <andrew.forrester.1@ulaval.ca>
2025-04-18 22:08:24 -0400

    sync

--- Projet2/python_scripts/analyse.py
+++ Projet2/python_scripts/analyse.py
@@ -1,16 +1,58 @@
-import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-
 """
 Author: Andrew Forrester
 Created: 2025-04-18
 Description: ...
 """
 
-def main() -> None:
-    pass
+import os
+import wave
+from glob import glob
+import numpy as np
+from scipy import signal
+from scipy.fft import fft, fftfreq
+from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
 
+
+def fourier(audio, sample_rate):
+    length = len(audio)
+    yf = fft(audio)
+    xf = fftfreq(length, 1/sample_rate)[:length//2]
+
+    plt.plot(xf, 2.0/length * np.abs(yf[0:length//2]))
+    plt.show()
+    
+def main() -> None:
+
+    # move into data directory
+    print(f"ran from {os.getcwd()}")
+    os.chdir("../donnees")
+    print(f"changed dir to {os.getcwd()}")
+
+    subdir = "preprocessed/manual_freq_sweep"
+    sample_rate = 40e3
+
+    files = glob(f"{subdir}/*.wav")
+    for file in files:
+        title = file.split('_')[-1].split('.')[0]
+        freq = float(title)
+        print(freq)
+    
+        a = read(file)
+        audio = np.array(a[1], dtype=float)
+        fourier(audio, sample_rate)
+
+
+##        fig = plt.figure()
+##       
+##        ax1 = fig.add_subplot(211)
+##        ax1.set_title(title)
+##        ax1.specgram(audio, NFFT=1024, Fs=sample_rate)
+##
+##        ax2 = fig.add_subplot(212)
+##        ax2.magnitude_spectrum(audio, Fs=sample_rate, scale='dB', color='C1')
+##        plt.show()
+    
 if __name__ == "__main__":
     main()
 
--- Projet2/python_scripts/preprocess_data.py
+++ Projet2/python_scripts/preprocess_data.py
@@ -1,22 +1,23 @@
-import os
-from glob import glob
-import numpy as np
-import scipy as sp
-import matplotlib.pyplot as plt
-
 """
 Author: Andrew Forrester
 Created: 2025-04-18
 Description: ...
 """
 
+import os
+import wave
+from glob import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def preprocess(raw_data: list[tuple]) -> np.array:
     """changes list of tuples to a ndarray"""
     time = [i[0] for i in raw_data]
     signal = [i[1] for i in raw_data]
 
-
     return np.array([time, signal])
+
 
 def main() -> None:
     """preprocesses the manual frequency sweep data"""
@@ -27,15 +28,27 @@ def main() -> None:
     print(f"changed dir to {os.getcwd()}")
 
     subdir = "raw_mokugo/manual_freq_sweep"
+    sample_rate = 40e3
 
     files = glob(f"{subdir}/*.npy")
 
     for file in files:
         save_file = file.replace("raw_mokugo", "preprocessed")
+        save_file_wave = save_file.replace("npy", "wav")
         raw_data = np.load(file)
 
         preprocessed_data = preprocess(raw_data)
-        np.save(save_file, preprocessed_data)
+##        np.save(save_file, preprocessed_data)
+
+        audio = preprocessed_data[1].T
+        audio = (audio * (2 ** 15 - 1)).astype("<h")
+
+        with wave.open(save_file_wave, "w") as f:
+            f.setnchannels(1)
+            # 2 bytes per sample.
+            f.setsampwidth(2)
+            f.setframerate(sample_rate)
+            f.writeframes(audio.tobytes())
 
 if __name__ == "__main__":
     main()
